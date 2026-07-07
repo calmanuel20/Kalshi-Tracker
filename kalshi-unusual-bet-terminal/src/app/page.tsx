7595dea4f373cd0a7c58d1c1f0424c17a21438bb
calmanuel20 <calmanuel20@gmail.com>
2026-07-07 16:42:12 -0500

    Delete kalshi-unusual-bet-terminal directory

--- kalshi-unusual-bet-terminal/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TerminalDashboard } from "@/components/TerminalDashboard";
-import { getDashboardData } from "@/lib/dashboard";
-
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  const data = await getDashboardData();
-  return <TerminalDashboard initialData={data} />;
-}
